d4d8a0504ee68b7b93ef8fc4a83ccc9024ead0d4
Amet-Han <velilyaev1999@bk.ru>
2020-06-02 23:34:24 +0300

    save

---
+++ Packages/libsass-build-sublime/README.md
@@ -0,0 +1,56 @@
+# *****DEPRECATED***** 
+
+Please check out [Libsass Build](https://github.com/blitzrk/sublime_libsass). It has all the features (and they're actually work) plus it has **no external dependencies**!!! It is also listed in Package Control.
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+# libsass-build-sublime
+
+Sublime Text build system for SASS using libsass and Node.js
+
+## Requirements
+
+You must have **Node.js** and **npm** installed.
+
+## Installation
+
+Depending on your OS, the location Sublime stores packages may change.
+
+For most *nix machines:
+
+```sh
+$ cd ~/.config/sublime-text-3/Packages
+$ git clone https://github.com/blitzrk/libsass-build-sublime.git
+```
+
+*Sublime Package Control integration coming soon...*
+
+## Usage
+
+Unless you have conflicting SASS build systems, libsass should be automatically selected, so set
+your build system to automatic in `Tools -> Build System -> Automatic` and compile with `ctrl+b`.
+
+### Troubleshooting
+
+If you have problems with npm, verify that:
+
+- `NODE_PATH` is set to your local node_modules folder
+- `NODE_PATH` points to path rw-accessible to the user
+- npm is installed at `NODE_PATH`'s location
+- node-gyp can be run by the user
+
+If your problems are specific to node-sass, try changing the version pointed to in `package.json`.
---
+++ Packages/libsass-build-sublime/build.js
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+
+// get required packages
+var i, p, log;
+var needsInstall = false;
+var npm = require('npm');
+npm.load({}, function(err, npm) {
+	if(err) throw new Error(
+		"Fatal error: This package requires Node.js and npm.\n" +
+		"Be sure to set your NODE_PATH to the node_modules directory" +
+		" where your globals and npm are installed."
+	);
+	npm.prefix = __dirname;
+
+	// ignore default console output
+	log = console.log;
+	console.log = function() {};
+
+	// check for not installed dependencies
+	npm.commands.outdated([], function(err, data) {
+		if(err) throw new Error(err);
+		for(i=0; i<data.length; i++) {
+			p = data[i];
+			// check not installed or not the desired version
+			if(!p[2] || p[2]!==p[3]) {
+				log("Warning: Installing dependencies. This may take awhile.");
+				needsInstall = true;
+				npm.commands.install([], compileToSass);
+				break;
+			}
+		}
+
+		// reset console
+		console.log = log;
+		if(!needsInstall) compileToSass();
+	});
+});
+
+function compileToSass(err, data) {
+	if(err) throw new Error("Could not install packages:\n" + err);
+
+	// load packages
+	var fs = require('fs');
+	var sass = require('node-sass');
+	var path = require('path');
+	var resolve = require('resolve');
+
+	// get arguments
+	var file = process.argv[2];
+	var settingsPath = process.argv[3];
+
+	// setup output and settings
+	var outFile = file.substr(0, file.lastIndexOf(".")) + ".css";
+	var settings;
+	try {
+		settings = JSON.parse(fs.readFileSync(settingsPath, 'utf8').replace(/\/\/.*|\n|\t/g,"").toString());
+	} catch(err) {
+		settingsPath = settingsPath.replace(/User/,"libsass-build-sublime");
+		settings = JSON.parse(fs.readFileSync(settingsPath, 'utf8').replace(/\/\/.*|\n|\t/g,"").toString());
+	}
+	var sassPaths = settings.includePaths.map(function(p) { return path.resolve('~', p) });
+
+	// find Node packages to include
+	settings.includePackages.forEach(function(m) {
+		try {
+			var res = resolve.sync(m, { basedir: process.cwd() });
+			var paths = require(res).includePaths;
+			if(!paths) throw new Error("Module " + m + " does not have an includePaths property.");
+	    	sassPaths = sassPaths.concat(paths);
+		} catch(err) {
+			console.log("Warning: " + err.message.replace(/ from \'\/.*/,"."));
+		}
+	});
+
+	// render CSS file
+	sass.renderFile({
+		"file": file,
+		includePaths: sassPaths,
+		"success": function(css) { },
+		"error": function(error){ throw new Error(error); },
+		"outFile": outFile
+	});
+}
